cff06f6f8c0e848d690d628e2dc6a8c9bbfe8ddc
Saimohan <amukantisaimohan@gmail.com>
2026-09-11 18:37:34 +0530

    Fix LayoutProps type error in layout.tsx and import Leaflet CSS globally

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, useMap, useMapEvents } from 'react-leaflet';
 import L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
+// import 'leaflet/dist/leaflet.css';
 import { Locality, Apartment, TransitPOI } from '../types';
 
 interface MapProps {
